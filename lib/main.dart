4cd48e66c210675a19d35bc01ba348e9accc202c
speedy <k.w1@o2.pl>
2021-01-16 22:16:57 +0100

    Implement recipe_repository

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:divide_and_cooquer/repositories/recipe_repository.dart';
 import 'package:divide_and_cooquer/view/app.dart';
 import 'package:flutter/material.dart';
 
@@ -6,8 +7,17 @@ import 'JsonPage.dart';
 void main() => runApp(App());
 
 class MyApp extends StatelessWidget {
+
+  void load() {
+    RecipeRepository recipeRepository = RecipeRepository();
+    recipeRepository.loadRecipes();
+  }
+
+
   @override
   Widget build(BuildContext context) {
+    load();
+
     return MaterialApp(
       title: 'Divide And Cooquer',
       theme: ThemeData(
--- lib/repositories/recipe_repository.dart
+++ lib/repositories/recipe_repository.dart
@@ -1,23 +1,55 @@
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:divide_and_cooquer/models/recipe.dart';
+import 'package:path_provider/path_provider.dart';
 
 class RecipeRepository {
   List<Recipe> recipes = [];
 
-  RecipeRepository() {
-    recipes
-      ..add(Recipe(name:"Schabowe"))
-      ..add(Recipe(name:"Mielone"));
-  }
+  RecipeRepository();
 
   Future<List<Recipe>> loadRecipes() async {
-    return recipes;
+    final myDir = new Directory(_localPath.toString());
+    if(myDir.listSync().length == 0) {
+      readRecipesOnline();
+    }
+    List<Recipe> recipeList;
+    myDir.list(recursive: true, followLinks: false)
+        .listen((FileSystemEntity entity) {
+
+      recipeList.addAll(createRecipiesFromFile(entity.path));
+    });
+
+    return recipeList;
+  }
+
+  Future<String> get _localPath async {
+    final directory = await getApplicationDocumentsDirectory();
+    return directory.path;
+  }
+
+  List<Recipe> createRecipiesFromFile(String path) {
+    final string = File(path).readAsStringSync();
+    final json = JsonDecoder().convert(string);
+    final List<Recipe> recipesList = (json)
+        .map((recipe) => Recipe(name:recipe["name"]))
+        .toList();
+    recipesList.forEach((element) {saveRecipe(element);});
+    return recipesList;
   }
 
   Future<void> saveRecipe(Recipe recipe) {
       recipes.add(recipe);
   }
 
-  Future<void> deleteRecipe(String name) {
+  Future<void> deleteRecipe(Recipe recipe) {
+    recipes.remove(recipe);
 
   }
+
+  void readRecipesOnline() {
+
+  }
+
 }
